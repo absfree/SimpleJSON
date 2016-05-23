d049b414007bd505f5a227a1ae97ee406afa385e
absfree <xiaoyaoyou818@outlook.com>
2016-05-23 10:36:24 +0800

    Update GsonTest.java

--- src/GsonTest.java
+++ src/GsonTest.java
@@ -13,14 +13,19 @@ import java.net.URL;
  */
 public class GsonTest {
     public static final String urlString = "http://news-at.zhihu.com/api/4/news/latest";
+   
     public static void main(String[] args) {
         LatestNews latest = new LatestNews();
         String jsonString = new String(HttpUtil.get(urlString));
+        
         long startTime = System.currentTimeMillis();
+        
         latest = (new Gson()).fromJson(jsonString, LatestNews.class);
+        
         long endTime = System.currentTimeMillis();
         double time = (double) (endTime - startTime) / 1000.0;
         System.out.println("took " + time + "seconds.");
+        
         System.out.println(latest.getDate());
         for (int i = 0; i < latest.getTop_stories().size(); i++) {
             System.out.println(latest.getTop_stories().get(i));
