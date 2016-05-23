4a8bf559796597ac6f0d1bd56cd3ff51efc74149
absfree <xiaoyaoyou818@outlook.com>
2016-05-23 10:39:21 +0800

    Update SimpleJSONTest.java

--- src/SimpleJSONTest.java
+++ src/SimpleJSONTest.java
@@ -9,11 +9,15 @@ public class SimpleJSONTest {
     public static final String urlString = "http://news-at.zhihu.com/api/4/news/latest";
     public static void main(String[] args) throws Exception {
         String jsonString = new String(HttpUtil.get(urlString));
+       
         long startTime = System.currentTimeMillis();
+       
         LatestNews latest = Parser.fromJson(jsonString, LatestNews.class);
+       
         long endTime = System.currentTimeMillis();
         double time = (double) (endTime - startTime) / 1000.0;
         System.out.println("took " + time + "seconds.");
+       
         System.out.println(latest.getDate());
         for (int i = 0; i < latest.getTop_stories().size(); i++) {
             System.out.println(latest.getTop_stories().get(i));
